b012d8c5d154120669db52d78793584bf0ba3c68
bondon <bondon1024@163.com>
2023-04-04 18:44:47 +0800

    hot-fix second idea commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Hello world2!");
         System.out.println("Hello world2!");
         System.out.println("Hello world4hot-fix!");
+        System.out.println("Hello world5hot-fix!");
+
     }
 }
