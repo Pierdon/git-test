e6f7ab61f0e6442fc1be454b7727040e078b2877
bondon <bondon1024@163.com>
2023-04-04 19:20:23 +0800

    master fourth push idea commit using ssh

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.println("push test!");
         System.out.println("push test2!");
         System.out.println("push test3!");
-
+        System.out.println("push test4!");
     }
 }
